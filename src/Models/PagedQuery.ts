b764a15b43c18179ac2acc55ec7bfb441e2dffc7
kadamczak <kadamczak.dev@gmail.com>
2025-11-25 18:38:27 +0100

    Made PagedQueryToParams more concise

--- src/Models/PagedQuery.ts
+++ src/Models/PagedQuery.ts
@@ -18,32 +18,21 @@ export const defaultPagedQuery: PagedQuery = {
 
 export const pagedQueryToParams = (query: PagedQuery, addPrefix: boolean): URLSearchParams => {
   const params = new URLSearchParams();
+  const prefix = addPrefix ? 'PagedQuery.' : '';
 
-  if (query.searchPhrase !== null && query.searchPhrase !== undefined) {
-    addPrefix
-    ? params.append('PagedQuery.searchPhrase', query.searchPhrase)
-    : params.append('searchPhrase', query.searchPhrase);
-  }
-  if (query.sortBy !== null && query.sortBy !== undefined) {
-    addPrefix
-    ? params.append('PagedQuery.sortBy', query.sortBy)
-    : params.append('sortBy', query.sortBy);
-  }
-  if (query.sortDirection !== undefined) {
-    addPrefix
-    ? params.append('PagedQuery.sortDirection', query.sortDirection)
-    : params.append('sortDirection', query.sortDirection);
-  }
-  if (query.pageNumber !== undefined) {
-    addPrefix
-    ? params.append('PagedQuery.pageNumber', query.pageNumber.toString())
-    : params.append('pageNumber', query.pageNumber.toString());
-  }
-  if (query.pageSize !== undefined) {
-    addPrefix
-    ? params.append('PagedQuery.pageSize', query.pageSize.toString())
-    : params.append('pageSize', query.pageSize.toString());
-  }
+  const entries: [string, string | number | undefined][] = [
+    ['searchPhrase', query.searchPhrase ?? undefined],
+    ['sortBy', query.sortBy ?? undefined],
+    ['sortDirection', query.sortDirection],
+    ['pageNumber', query.pageNumber],
+    ['pageSize', query.pageSize],
+  ];
+
+  entries.forEach(([key, value]) => {
+    if (value !== null && value !== undefined) {
+      params.append(prefix + key, value.toString());
+    }
+  });
 
   return params;
 };
