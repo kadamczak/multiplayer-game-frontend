4906e5b8d1b740398f9e047f89cc07f425c98763
kadamczak <kadamczak.dev@gmail.com>
2025-11-27 17:34:20 +0100

    Clear HomePage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -15,12 +15,6 @@ const HomePage = () => {
       <p className={styles.description}>
         Join players from around the world in an exciting gaming experience.
       </p>
-
-      <img src="/gamescreenshot1.png" alt="Gaming Illustration" className={styles.illustration} />
-
-      <p className={styles.description}>
-        Create an account or log in to get started! Haha funny video game! 
-      </p>
     </div>
   )
 }
