89a662e9b72541680524b2441a7ade095de45e0b
kadamczak <kadamczak.dev@gmail.com>
2025-12-25 20:51:25 +0100

    Begin implementing multilanguage support

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
 import './index.css'
+import './i18n'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  //<StrictMode>
     <RouterProvider router={router} />
-  //</StrictMode>,
 )
